fea28ac66cd146d8100fd87c9ce3073153950e52
lenis96 <lenisandres5@gmail.com>
2020-02-03 21:09:18 -0500

    upload xor model

--- apps/predictions/views.py
+++ apps/predictions/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import pickle
+from sklearn.neural_network import MLPClassifier
 # Create your views here.
 
 class PredictionAPIView(APIView):
     def get(self,request,prediction):
-        return Response({'message':'ok'})
+        model = pickle.load(open('models/xor.sav','rb'))
+        res = model.predict([[0,0],[0,1],[1,0],[1,1]])
+        return Response({'message':'ok','res':res})
